188057a9fd0e958312d5ca90703bf8ac979233c9
fandy1404 <136670174+fandy1404@users.noreply.github.com>
2025-11-24 08:51:00 +0800

    Update DatabaseHelper.java

--- app/src/main/java/com/example/daeptv/DatabaseHelper.java
+++ app/src/main/java/com/example/daeptv/DatabaseHelper.java
@@ -5,17 +5,19 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "media.db";
+
+    private static final String DATABASE_NAME = "media_metadata.db";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_MEDIA = "media";
     private static final String COLUMN_ID = "id";
-    private static final String COLUMN_TYPE = "type"; // "audio" atau "video"
-    private static final String COLUMN_PATH = "path"; // Path file, misalnya "audio1.mp3"
-    private static final String COLUMN_NAME = "name"; // Nama file
+    private static final String COLUMN_TYPE = "type";
+    private static final String COLUMN_PATH = "path";
+    private static final String COLUMN_NAME = "name";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,12 +25,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_MEDIA + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_TYPE + " TEXT, " +
-                COLUMN_PATH + " TEXT, " +
-                COLUMN_NAME + " TEXT)";
-        db.execSQL(createTable);
+        db.execSQL("CREATE TABLE " + TABLE_MEDIA + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_TYPE + " TEXT," +
+                COLUMN_PATH + " TEXT," +
+                COLUMN_NAME + " TEXT)");
     }
 
     @Override
@@ -37,31 +38,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    // Method untuk menyimpan media baru (dipanggil dari form admin)
     public void addMedia(String type, String path, String name) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TYPE, type);
-        values.put(COLUMN_PATH, path);
-        values.put(COLUMN_NAME, name);
-        db.insert(TABLE_MEDIA, null, values);
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TYPE, type);
+        cv.put(COLUMN_PATH, path);
+        cv.put(COLUMN_NAME, name);
+        db.insert(TABLE_MEDIA, null, cv);
         db.close();
     }
 
-    // Method untuk mengambil semua media sebagai JSON (untuk dikirim ke WebView)
     public String getAllMediaAsJson() {
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_MEDIA, null);
-        JSONArray jsonArray = new JSONArray();
+        JSONArray arr = new JSONArray();
         if (cursor.moveToFirst()) {
             do {
-                JSONObject obj = new JSONObject();
                 try {
+                    JSONObject obj = new JSONObject();
                     obj.put("id", cursor.getInt(cursor.getColumnIndex(COLUMN_ID)));
                     obj.put("type", cursor.getString(cursor.getColumnIndex(COLUMN_TYPE)));
                     obj.put("path", cursor.getString(cursor.getColumnIndex(COLUMN_PATH)));
                     obj.put("name", cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
-                    jsonArray.put(obj);
+                    arr.put(obj);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -69,6 +68,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         cursor.close();
         db.close();
-        return jsonArray.toString();
+        return arr.toString();
     }
 }
